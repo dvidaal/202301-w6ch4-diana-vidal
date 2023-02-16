9a9179c4b16b0b17d4344932d5cc1409477eeb74
Diana Vidal <dvidaal@gmail.com>
2023-02-16 19:50:43 +0100

    Create structure and array for things I know

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,10 @@ module.exports = {
     {
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/consistent-type-definitions": "off",
+        "@typescript-eslint/naming-convention": "off",
+      },
     },
   ],
   parserOptions: {
